160ae2c3edd0c9f6aaa4679f10cc92e092daa1d0
zuoqiang <756465185@qq.com>
2018-04-23 00:04:26 +0800

    格式更正

--- src/main/java/com/zuoqiang/test/shuthook/ShutdownHookTest.java
+++ src/main/java/com/zuoqiang/test/shuthook/ShutdownHookTest.java
@@ -7,16 +7,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * 测试 ShutdownHook
  * 1）直接杀掉程序; 2)正常退出的时候; 3)抛出异常的时候
+ *
  * @author zuoqiang
  */
 
 public class ShutdownHookTest {
-    //定时器
+    /**
+     * 定时器
+     */
     static Timer timer = new Timer("job_time");
-    //统计次数
+
+    /**
+     * 统计次数
+     */
     static AtomicInteger count = new AtomicInteger(0);
 
-    //钩子hook线程
+    /**
+     * 钩子hook线程
+     */
     static class CleanThread extends Thread {
         @Override
         public void run() {
