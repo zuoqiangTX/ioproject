d7ef66c82fe196f3bd77524bacdefdf2977e984c
zuoqiang <756465185@qq.com>
2018-04-22 23:42:30 +0800

    ShutdownHook 钩子，用来在程序结束的时候平滑退出，关掉没用的资源。

---
+++ src/main/java/com/zuoqiang/test/shuthook/ShutdownHookTest.java
@@ -0,0 +1,49 @@
+package com.zuoqiang.test.shuthook;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 测试 ShutdownHook
+ * @author zuoqiang
+ */
+
+public class ShutdownHookTest {
+    //定时器
+    static Timer timer = new Timer("job_time");
+    //统计次数
+    static AtomicInteger count = new AtomicInteger(0);
+
+    //钩子hook线程
+    static class CleanThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("CLEAN SOME WORK.");
+            timer.cancel();
+            try {
+                Thread.sleep(2 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //将钩子注册到运行时环境中去
+        Runtime.getRuntime().addShutdownHook(new CleanThread());
+        System.out.println("MAIN CLASS START......");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                count.getAndIncrement();
+                System.out.println("doing job" + count);
+                if (count.get() == 10) {
+                    //int a = 1 / 0;
+                    //退出程序
+                    System.exit(0);
+                }
+            }
+        }, 0, 2 * 1000);
+    }
+}
