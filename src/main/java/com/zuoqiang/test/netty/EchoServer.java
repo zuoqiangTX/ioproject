92f152c3349a1e4478c901e7968e63a1e3fe4f98
baiyue <baiyue@tongbanjie.com>
2018-05-11 15:42:54 +0800

    First Netty[Echo client] ps: 注意args要配置port,host

--- src/main/java/com/zuoqiang/test/netty/EchoServer.java
+++ src/main/java/com/zuoqiang/test/netty/EchoServer.java
@@ -29,16 +29,21 @@ public class EchoServer {
         final EchoServerHandler serverHandler = new EchoServerHandler();
         EventLoopGroup loopGroup = new NioEventLoopGroup();
         try {
+            //创建实例引导和绑定服务器
             ServerBootstrap bootstrap = new ServerBootstrap();
+            //创建一个EventLoopGroup实例来进行事件的处理（接收新连接、读/写数据）
             bootstrap.group(loopGroup)
                     .channel(NioServerSocketChannel.class)
+                    //指定服务器绑定本地的port
                     .localAddress(new InetSocketAddress(port))
+                    //使用serverHandler实例化每一个新的Channel
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
                             socketChannel.pipeline().addLast(serverHandler);
                         }
                     });
+            //绑定服务器，sync将会一直阻塞直到完成
             ChannelFuture future = bootstrap.bind().sync();
             future.channel().closeFuture().sync();
         } finally {
