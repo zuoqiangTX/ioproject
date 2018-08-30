8555dc658fa7186c9e8e253abf3e40241e6a4343
zuoqiang <756465185@qq.com>
2018-08-30 23:46:40 +0800

    Marshalling 序列化技术

---
+++ src/main/java/com/zuoqiang/test/until/PathConstans.java
@@ -0,0 +1,9 @@
+package com.zuoqiang.test.until;
+
+import java.io.File;
+
+public class PathConstans {
+
+    public static String myObjectchar = File.separatorChar + "src" + File.separatorChar + "main" + File.separatorChar + "resources";
+
+}
