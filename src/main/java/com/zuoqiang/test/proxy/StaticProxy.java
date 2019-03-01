69b258be20865dfc964b783d69fbd16f0fb421c1
baiyue <baiyue@tongbanjie.com>
2019-03-01 16:35:57 +0800

    静态代理 1、实现目标类的接口 2、持有目标类的引用

---
+++ src/main/java/com/zuoqiang/test/proxy/StaticProxy.java
@@ -0,0 +1,13 @@
+package com.zuoqiang.test.proxy;
+
+/**
+ * 静态代理 代理类（1、实现目标类的接口 2、持有目标类的对象）
+ *
+ * @author zuoqiang
+ */
+public class StaticProxy {
+    public static void main(String[] args) {
+        IUserDao userDao = new UserDaoProxy();
+        userDao.save();
+    }
+}
---
+++ src/main/java/com/zuoqiang/test/proxy/UserDaoProxy.java
@@ -0,0 +1,26 @@
+package com.zuoqiang.test.proxy;
+
+/**
+ * 静态代理
+ * 特点：
+ * 2. 目标对象必须要实现接口
+ * 2. 代理对象，要实现与目标对象一样的接口
+ */
+class UserDaoProxy implements IUserDao {
+    /**
+     * 代理对象，需要维护一个目标对象
+     */
+    private UserDao target = new UserDao();
+
+    @Override
+    public void save() {
+        System.out.println("代理操作： 开启事务...");
+        target.save();   // 执行目标对象的方法
+        System.out.println("代理操作：提交事务...");
+    }
+
+    @Override
+    public void find() {
+        target.find();
+    }
+}
