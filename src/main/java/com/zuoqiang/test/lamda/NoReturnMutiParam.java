54a01927a7812ceb85388e28552f36b1722ae1a9
zuoqiang <756465185@qq.com>
2020-03-04 15:56:38 +0800

    lamda基础语法

---
+++ src/main/java/com/zuoqiang/test/lamda/NoReturnMutiParam.java
@@ -0,0 +1,11 @@
+package com.zuoqiang.test.lamda;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @date 2020-03-04 15:27
+ */
+@FunctionalInterface
+public interface NoReturnMutiParam {
+    void test(int a, int b);
+}
---
+++ src/main/java/com/zuoqiang/test/lamda/NoReturnNoParam.java
@@ -0,0 +1,11 @@
+package com.zuoqiang.test.lamda;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @date 2020-03-04 15:26
+ */
+@FunctionalInterface
+public interface NoReturnNoParam {
+    void test();
+}
