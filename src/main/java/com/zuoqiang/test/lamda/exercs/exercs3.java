8a041d84797b7ac7de363551eacd708144003c60
zuoqiang <756465185@qq.com>
2020-03-06 23:05:45 +0800

    lamda案例-集合删除和遍历

---
+++ src/main/java/com/zuoqiang/test/lamda/exercs/exercs3.java
@@ -0,0 +1,29 @@
+package com.zuoqiang.test.lamda.exercs;
+
+import com.google.common.collect.Lists;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 集合遍历 foreach
+ *
+ * @author zuoqiang
+ * @version 1.0
+ * @date 2020-03-06 22:28
+ */
+public class exercs3 {
+    public static void main(String[] args) {
+        List<Integer> list = Lists.newArrayList();
+        Collections.addAll(list, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+        //将集合的每一个元素都带入到方法accept中
+//        list.forEach(System.out::println);
+
+//输出集合中所有的偶数
+        list.forEach(ele -> {
+            if (ele % 2 == 0) {
+                System.out.println(ele);
+            }
+        });
+    }
+}
