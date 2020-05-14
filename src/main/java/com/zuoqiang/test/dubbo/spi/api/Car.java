2ff8fde41620e773e68658dcc768c4b2fa5b476a
zuoqiang <zuoqiang@lianlianpay.com>
2020-05-14 16:08:59 +0800

    简单的dubbo spi机制 service 文件里面是 key-value

---
+++ src/main/java/com/zuoqiang/test/dubbo/spi/api/Car.java
@@ -0,0 +1,15 @@
+package com.zuoqiang.test.dubbo.spi.api;
+
+import org.apache.dubbo.common.extension.SPI;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @description 接口
+ * @date 2020/5/14 4:00 下午
+ */
+
+@SPI
+public interface Car {
+    void getColor();
+}
---
+++ src/main/java/com/zuoqiang/test/dubbo/spi/impl/BlackCar.java
@@ -0,0 +1,17 @@
+package com.zuoqiang.test.dubbo.spi.impl;
+
+import com.zuoqiang.test.dubbo.spi.api.Car;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @description todo
+ * @date 2020/5/14 4:03 下午
+ */
+
+public class BlackCar implements Car {
+    @Override
+    public void getColor() {
+        System.out.println("black");
+    }
+}
