28ec45f38bc8c7b2548304678cdb27e96f05dd7c
zuoqiang <zuoqiang@lianlianpay.com>
2021-04-12 03:51:44 +0800

    广度优先算法（层序遍历）

---
+++ src/main/java/com/zuoqiang/test/leetcode/tree/cengxu/Solution.java
@@ -0,0 +1,59 @@
+package com.zuoqiang.test.leetcode.tree.cengxu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @description todo
+ * @date 2021/4/12 3:45 上午
+ */
+
+public class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+
+        }
+        return res;
+    }
+}
