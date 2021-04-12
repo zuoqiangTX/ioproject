9f7c4487269428fe5fa3f32553e0e0d3f437e063
zuoqiang <zuoqiang@lianlianpay.com>
2021-04-12 15:51:56 +0800

    快速排序

---
+++ src/main/java/com/zuoqiang/test/leetcode/sort/QuickSort.java
@@ -0,0 +1,55 @@
+package com.zuoqiang.test.leetcode.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @description 快速排序
+ * @date 2021/4/12 3:29 下午
+ */
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 2, 4, 7, 62, 3, 4, 2, 1, 8, 9, 19};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void quickSort(int[] array, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        // base中存放基准数
+        int base = array[left];
+        int i = left, j = right;
+        while (i != j) {
+            // 顺序很重要，先从右边开始往左找，直到找到比base值小的数
+            while (array[j] >= base && i < j) {
+                j--;
+            }
+
+            // 再从左往右边找，直到找到比base值大的数
+            while (array[i] <= base && i < j) {
+                i++;
+            }
+
+            // 上面的循环结束表示找到了位置或者(i>=j)了，交换两个数在数组中的位置
+            if (i < j) {
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+
+        // 将基准数放到中间的位置（基准数归位）
+        array[left] = array[i];
+        array[i] = base;
+
+        // 递归，继续向基准的左右两边执行和上面同样的操作
+        // i的索引处为上面已确定好的基准值的位置，无需再处理
+        quickSort(array, left, i - 1);
+        quickSort(array, i + 1, right);
+    }
+}
