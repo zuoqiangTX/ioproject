37060ee75e9ba1fdf5e009a6df0bbdeb4b64b1cc
zuoqiang <zuoqiang@lianlianpay.com>
2021-04-13 22:02:30 +0800

    fix

--- src/main/java/com/zuoqiang/test/leetcode/Test.java
+++ src/main/java/com/zuoqiang/test/leetcode/Test.java
@@ -1,11 +1,17 @@
 package com.zuoqiang.test.leetcode;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import javafx.util.Pair;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author zuoqiang
@@ -36,7 +42,9 @@ import java.util.List;
 public class Test {
     @Data
     public static class Ticket {
+        //大小1-13
         private int num;
+        //花色 1-4
         private int color;
 
         public Ticket(int num, int color) {
@@ -55,6 +63,11 @@ public class Test {
     }
 
     public static class Game {
+
+        private static final String ROYAL_FLUSH = "13121110";
+        private static final int PLAYER_ONE_BIG = 1;
+        private static final int PLAYER_TWO_BIG = -1;
+        private static final int PLAYER_SANE = 0;
 
         /**
          * 计算扑克牌的大小
@@ -86,15 +99,197 @@ public class Test {
                 }
             });
             //认为一样大
-
-
-            return 0;
+            return compare(t1, t2);
         }
+
+        /**
+         * 比较逻辑
+         *
+         * @param t1
+         * @param t2
+         * @return 0 一样 正数player1大 负数player2 大
+         */
+        private static int compare(List<Ticket> t1, List<Ticket> t2) {
+            //先比较是否特殊情况
+            String t1Str = transferNumToString(t1);
+            String t2Str = transferNumToString(t2);
+            int[] t1Array = t1.stream().mapToInt(Ticket::getNum).toArray();
+            int t1Sum = t1.stream().mapToInt(Ticket::getNum).sum();
+            int t2Sum = t1.stream().mapToInt(Ticket::getNum).sum();
+            int[] t2Array = t2.stream().mapToInt(Ticket::getNum).toArray();
+            if (t1Str.equals(t2Str) && isSameColor(t1Array) && isSameColor(t2Array)) {
+                //花色&&大小相同
+                return PLAYER_SANE;
+            }
+            //---RoyalFlush-----只能是AKQJT，且同花-------
+            if (isRoyalFlush(t1, t1Str)) {
+                //t1大
+                return PLAYER_ONE_BIG;
+            } else if (isRoyalFlush(t2, t1Str)) {
+                return PLAYER_TWO_BIG;
+            }
+            //------Flush Straight: 是同花，且是顺子（5432A是最小的顺子，最大的为AKQJT）---
+            // 都是顺子(花色相同大小相同前面已经过滤了)
+            if (isStraight(t1) && isStraight(t2)) {
+                //1)先比较顺子大(直接计算大小的和，哪个大哪个就是（因为已经是顺子了）)
+                if (t1Sum == t1Sum) {
+                    Boolean sameColorT1 = isSameColor(t1Array);
+                    if (sameColorT1) {
+                        return PLAYER_ONE_BIG;
+                    } else {
+                        return PLAYER_TWO_BIG;
+                    }
+                } else {
+                    return t1Sum - t2Sum;
+                }
+            }
+            //------Four of a Kind: AAAAx（x表示任意牌, A表示四张一样大小的牌，如果两手牌都是Four of a Kind，则比较A的大小）
+            Pair<Boolean, Integer> t1Pair = fourAndKind(t1);
+            Pair<Boolean, Integer> t2Pair = fourAndKind(t2);
+            if (t1Pair.getKey() && t2Pair.getKey()) {
+                Integer t1Num = t1Pair.getValue();
+                Integer t2Num = t2Pair.getValue();
+                return t1Num.compareTo(t2Num);
+            } else if (t1Pair.getKey() && !t2Pair.getKey()) {
+                return PLAYER_ONE_BIG;
+            } else if (t2Pair.getKey() && !t1Pair.getKey()) {
+                return PLAYER_TWO_BIG;
+            }
+            //---Full House: AAABB（A表示3张一样大小的牌，B表示2张一样大小的牌，如果两手牌都是Full House，则比较A的大小，如果两手牌都是Full Hose且A相同，则比较B的大小）
+            fourHouse(t1);
+            return PLAYER_SANE;
+        }
+
+        /**
+         * @param tickets
+         * @return
+         */
+        public static Triple<Boolean, Boolean, Integer> fourHouse(List<Ticket> tickets) {
+            boolean hasThree = false;
+            boolean hasTwo = false;
+            Integer threeNum = null;
+            List<Integer> collect = tickets.stream().map(Ticket::getNum).collect(Collectors.toList());
+            Map<String, Integer> countMap = Maps.newHashMap();
+            collect.forEach(e -> {
+                if (countMap.containsKey(e.toString())) {
+                    Integer num = countMap.get(e);
+                    countMap.put(e.toString(), num + 1);
+                } else {
+                    countMap.put(e.toString(), 0);
+                }
+            });
+            //判断hashmap是否有一个值为4的key,有的话返回值，否则返回false;
+            for (String key : countMap.keySet()) {
+                int num = countMap.get(key);
+                if (num == 3) {
+                    hasThree = true;
+                    threeNum = Integer.parseInt(key);
+                }
+                if (num == 2) {
+                    hasTwo = true;
+                }
+            }
+            return Triple.of(hasThree, hasTwo, threeNum);
+        }
+
+
+        /**
+         * 判断是否为 A表示四张一样大小的牌并且返回对应的牌
+         *
+         * @param tickets
+         * @return
+         */
+        public static Pair<Boolean, Integer> fourAndKind(List<Ticket> tickets) {
+            List<Integer> collect = tickets.stream().map(Ticket::getNum).collect(Collectors.toList());
+            Map<String, Integer> countMap = Maps.newHashMap();
+            collect.forEach(e -> {
+                if (countMap.containsKey(e.toString())) {
+                    Integer num = countMap.get(e);
+                    countMap.put(e.toString(), num + 1);
+                } else {
+                    countMap.put(e.toString(), 0);
+                }
+            });
+            //判断hashmap是否有一个值为4的key,有的话返回值，否则返回false;
+            for (String key : countMap.keySet()) {
+                int num = countMap.get(key);
+                if (num == 4) {
+                    return new Pair<>(true, Integer.parseInt(key));
+                }
+            }
+            return new Pair<>(false, 0);
+        }
+
+        /**
+         * 判断是否为RoyalFlush
+         * 只能是AKQJT，且同花
+         *
+         * @param t
+         * @param tStr
+         * @return
+         */
+        private static boolean isRoyalFlush(List<Ticket> t, String tStr) {
+            return ROYAL_FLUSH.equals(tStr) && isSameColor(t.stream().mapToInt(Ticket::getNum).toArray());
+        }
+
+        /**
+         * 是否顺子
+         *
+         * @param
+         * @param t
+         * @return
+         */
+        private static boolean isStraight(List<Ticket> t) {
+            //todo 判断是否为顺子
+            return true;
+        }
+
+        /**
+         * 是否同色
+         *
+         * @param nums
+         * @return
+         */
+        public static Boolean isSameColor(int[] nums) {
+            int res = 0;
+            for (int i : nums) {
+                res ^= i;
+            }
+            if (res == 0) {
+                //相同的数字异或为0
+                return true;
+            }
+            return false;
+        }
+
+
+        /**
+         * 转换牌为字符串
+         *
+         * @param tickets
+         * @return
+         */
+        private static String transferNumToString(List<Ticket> tickets) {
+            return StringUtils.join(tickets.stream().map(Ticket::getNum).collect(Collectors.toList()), "");
+        }
+
+        /**
+         * 转换花色为字符串
+         *
+         * @param tickets
+         * @return
+         */
+        private static String transferColorToString(List<Ticket> tickets) {
+            return StringUtils.join(tickets.stream().map(Ticket::getColor).collect(Collectors.toList()), "");
+        }
+
     }
 
 
     public static void main(String[] args) {
         List<Ticket> ticket1 = Lists.newArrayList();
+        ticket1.add(new Ticket(1, 0));
+        ticket1.add(new Ticket(2, 0));
         //todo 省略加入player1的牌
         Player player1 = new Player(ticket1);
         List<Ticket> ticket2 = Lists.newArrayList();
