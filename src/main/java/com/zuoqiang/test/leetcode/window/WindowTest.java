13de819be794da834e363ae05ef9795238850cb0
zuoqiang <zuoqiang@lianlianpay.com>
2021-06-09 21:32:12 +0800

    滑动窗口模板

---
+++ src/main/java/com/zuoqiang/test/leetcode/window/WindowTest.java
@@ -0,0 +1,28 @@
+package com.zuoqiang.test.leetcode.window;
+
+/**
+ * @author zuoqiang
+ * @version 1.0
+ * @description 滑动窗口模板
+ * <p>
+ *       ArrayList s = Lists.newArrayList(1, 2, 3, 4);
+ *         int left = 0;
+ *         int right = 0;
+ *         while (right < s.size()) {
+ *             window.add(s[right]);
+ *             right++;
+ *             while (window) need shark){
+ *               //缩小窗口
+ *                 window.remove(s[left]);
+ *                 left++;
+ *             }
+ *         }
+ * </p>
+ * @date 2021/6/9 9:28 下午
+ */
+
+public class WindowTest {
+    public static void main(String[] args) {
+
+    }
+}
